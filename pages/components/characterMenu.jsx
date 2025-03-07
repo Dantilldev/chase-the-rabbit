6cbcb90b0fe7da0fb0d17ce104d27932ff6df213
Your Name <you@example.com>
2025-03-07 09:24:59 +0100

    feat: Add CharacterMenu component and toggle visibility in Home page

---
+++ pages/components/characterMenu.jsx
@@ -0,0 +1,13 @@
+export default function CharacterMenu() {
+  return (
+    <div className="text-white">
+      <h1>Characters</h1>
+      <div className="flex gap-4">
+        <div className="flex w-56 h-56 p-10 bg-white rounded-2xl">
+          Hello world
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import Link from "next/link";
+import CharacterMenu from "./components/characterMenu"; // Import the CharacterMenu component
 
 import { useState, useContext } from "react";
 
 export default function Home() {
+  const [showCharacters, setShowCharacters] = useState(false);
+
   return (
     <div
-      className="lex flex-col justify-center items-center min-h-screen w-full bg-cover bg-center"
+      className="lex flex-col justify-center items-center min-h-screen w-full bg-cover bg-center "
       style={{ backgroundImage: "url('/bg-imageV2.jpg')" }}
     >
       {/* <Game /> */}
-
-      <Link href="/game">
-        <button className="bg-blue-500 p-2 text-white font-bold rounded-2xl">
-          {" "}
-          play Game
-        </button>
-      </Link>
+      <div className="h-[600px] w-full flex flex-col justify-center items-center">
+        <div className="flex gap-4">
+          <Link href="/game">
+            <button className="bg-blue-500 rounded-xl text-center p-2 text-white font-bold cursor-pointer hover:scale-105 duration-150 ease-in transition-all w-32">
+              {" "}
+              play Game
+            </button>
+          </Link>
+          <button
+            onClick={() => setShowCharacters(!showCharacters)}
+            className="bg-orange-500 rounded-xl text-center p-2 text-white font-bold cursor-pointer hover:scale-105 duration-150 ease-in transition-all w-32"
+          >
+            {" "}
+            Charaters{" "}
+          </button>
+        </div>
+        {showCharacters && <CharacterMenu />}
+      </div>
     </div>
   );
 }
