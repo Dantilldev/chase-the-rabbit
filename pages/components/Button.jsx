c59ab3a5537ace2d38964c5f88ca356ae2992545
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-03-26 22:42:28 +0100

    feat: Update Button component to use legacy behavior in Link

--- pages/components/Button.jsx
+++ pages/components/Button.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-export default function Button({ href, text, onClick }) {
+export default function Button({href, text, onClick}) {
   return (
-    <Link href={href}>
+    <Link legacyBehavior href={href}>
       <button
         onClick={onClick}
         className="bg-teal-600 hover:bg-teal-500 text-white px-8 py-4 text-2xl rounded-lg transition-transform hover:scale-105 cursor-pointer"
